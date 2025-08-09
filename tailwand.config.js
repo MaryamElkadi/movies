fc18ef096f72d49894d5b4dbf9b28829ab39048b
maryam <maryam.khamis.elkadi@gmail.com>
2025-08-09 19:47:19 +0300

    Merge remote changes

---
+++ tailwand.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff2c1f',
+        'text-color': '#020307',
+        'bg-color': '#fff',
+      },
+      spacing: {
+        '4.5': '1.125rem',
+      },
+      borderRadius: {
+        '5xl': '5rem',
+      },
+      scrollPadding: {
+        'top': '2rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
